d19acb900559bcb442a36a23e76a23ef5c835428
Luiz Paes <sttdiel.luiz@gmail.com>
2023-08-29 16:32:54 -0300

    thiago gonçalves

--- aifood-app/src/Components/WelcomeButton.js
+++ aifood-app/src/Components/WelcomeButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
@@ -10,12 +10,20 @@ import LogoutIcon from '@mui/icons-material/Logout';
 const WelcomeButton = () => {
   const user = JSON.parse(localStorage.getItem('user'));
   const [anchorEl, setAnchorEl] = useState(null);
+  const [avatarColor, setAvatarColor] = useState(null);
 
   const getRandomColor = () => {
     const colors = [deepOrange[500], deepPurple[500], teal[500], indigo[500], pink[500], red[500]];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
   };
+
+  useEffect(() => {
+    if (user && !avatarColor) {
+      const color = getRandomColor();
+      setAvatarColor(color);
+    }
+  }, [user, avatarColor]);
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -28,7 +36,7 @@ const WelcomeButton = () => {
   const handleLogout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
-    window.location.href = '/'; // Recarrega a página para limpar o estado
+    window.location.href = '/';
   };
 
   return (
@@ -37,10 +45,10 @@ const WelcomeButton = () => {
         variant="standard"
         color="primary"
         onClick={handleMenuOpen}
-        startIcon={<Avatar sx={{ bgcolor: getRandomColor() }}>{user && user.name.charAt(0)}</Avatar>}
+        startIcon={<Avatar sx={{ bgcolor: avatarColor }}>{user && user.name.charAt(0)}</Avatar>}
         sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}
       >
-        <p style={{ color: 'white', margin: 0 }} >Olá {user && user.name} Bem-vindo!</p>
+        <p style={{ color: 'white', margin: 0 }}>Olá {user && user.name} Bem-vindo!</p>
       </Button>
       <Menu
         anchorEl={anchorEl}
